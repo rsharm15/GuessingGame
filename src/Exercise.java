aba17f36730004ec2ee93ac9a9803a75769d1cd2
rsharm15 <rahul.sharma@coveros.com>
2016-07-18 22:55:51 -0400

    Comment Change

--- src/Exercise.java
+++ src/Exercise.java
@@ -6,7 +6,7 @@
  * value to start. From that point, the program attempts to guess the number that the user has in his/her head. 
  * A number is shown a number on the screen and then the user is asked to enter "yes","end","lower" or "higher."
  * The "yes" or "end" indicate that the correct number is displayed on the screen. The "higher" means that the 
- * value that the user had in mind is higher than what is displayed. The "lower" means that the value that the 
+ * value that the user had in mind is higher than the number that is displayed. The "lower" means that the value that the 
  * user had in mind is lower than what is displayed.
  */
 
